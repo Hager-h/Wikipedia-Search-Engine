6bd376e3e32d5f6377abc11abda19b2c6bc8a7fb
Hager-h <125830198+Hager-h@users.noreply.github.com>
2023-04-11 23:49:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let resultsContainer = document.getElementsByClassName("container")[0];
+
+const validateInput = (el) => {
+  if (el.value === "") {
+    resultsContainer.innerHTML =
+      "<p>Type something in the above search input</p>";
+  } else {
+    debouncedRes(el.value, el);
+  }
+};
+
+const generateResults = (searchValue, inputField) => {
+  fetch(
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=" +
+      searchValue
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let results = data.query.search;
+      let numberOfResults = data.query.search.length;
+      resultsContainer.innerHTML = "";
+      for (let i = 0; i < numberOfResults; i++) {
+        let result = document.createElement("div");
+        result.classList.add("results");
+        result.innerHTML = `
+            <div>
+                <h3>${results[i].title}</h3>
+                <p>${results[i].snippet}</p>
+            </div>
+            <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
+            `;
+        resultsContainer.appendChild(result);
+      }
+      if (inputField.value === "") {
+        resultsContainer.innerHTML =
+          "<p>Type something in the above search input</p>";
+      }
+    });
+};
+// debounce function
+const debounce = (functionName, delay) => {
+  let timer;
+  return function (...arguments) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      functionName(...arguments);
+    }, delay);
+  };
+};
+
+const debouncedRes = debounce(generateResults, 900);
